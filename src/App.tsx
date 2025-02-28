af989569c8927b9df02f6f7a7833de64893b1c2c
Fatorres <ftorres2018@alu.uct.cl>
2025-02-27 23:55:48 -0300

    feature add: Weather app status ok, validation with Api consults, add Zod for types and finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import Form from "./components/Form/Form";
 import styles from "./App.module.css";
 import useWeather from "./hooks/useWeather";
+import { WeatherDetail } from "./components/WeatherDetail/WeatherDetail";
+import { Spinner } from "./components/Spinner/Spinner";
+import Alert from "./components/Alert/Alert";
 
 function App() {
 
-  const { fetchWeather } = useWeather();
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather();
 
-  console.log(import.meta.env);
+  // console.log(import.meta.env);
 
   return (
     <>
       <h1 className={styles.title}> Buscador de Clima </h1>
-
       <div className={styles.container}>
         <Form
           fetchWeather={fetchWeather}
-        
         />
-        <p>2</p>
+        {loading && <Spinner />}
+        {hasWeatherData && 
+        <WeatherDetail weather={weather} />
+        }
+        {notFound && <Alert> Ciudad no encontrada! </Alert>}
+        
       </div>
     </>
   );
